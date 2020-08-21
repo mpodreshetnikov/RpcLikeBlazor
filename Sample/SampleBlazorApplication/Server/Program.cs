090c05cf74a280ad715eaf7bdb44a5826caedaac
Maxim Podreshetnikov <maksim_podreshetnikov@outlook.com>
2020-08-21 11:12:41 +0700

    Update dependencies.

--- Sample/SampleBlazorApplication/Server/Program.cs
+++ Sample/SampleBlazorApplication/Server/Program.cs
@@ -12,9 +12,6 @@ namespace SampleBlazorApplication.Server
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
